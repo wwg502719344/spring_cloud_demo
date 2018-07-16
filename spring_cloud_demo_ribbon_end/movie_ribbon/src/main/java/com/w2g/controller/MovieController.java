f64f6d7574e2602e7a0926052dd13c7c69e47e76
502719344 <502719344@qq.com>
2018-07-16 17:31:26 +0800

    riddon最终版

---
+++ spring_cloud_demo_ribbon_end/movie_ribbon/src/main/java/com/w2g/controller/MovieController.java
@@ -0,0 +1,42 @@
+package com.w2g.controller;
+
+import com.w2g.config.RibbonConfiguration;
+import com.w2g.entity.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+//@RibbonClient(name = "user-ribbon", configuration = RibbonConfiguration.class)    //改为yml配置
+public class MovieController {
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @Autowired
+  private LoadBalancerClient loadBalancerClient;
+
+  private static Logger log = LoggerFactory.getLogger(UserInfo.class);
+
+  @GetMapping("/user/{id}")
+  public UserInfo findById(@PathVariable int id) {
+    return this.restTemplate.getForObject("http://user-ribbon/test", UserInfo.class);
+  }
+
+  @GetMapping("/log-instance")
+  public void showinstance() {
+    //获取进行负载均衡器中user_ribbon注册器下的实例
+    //后期可改造为官网推荐的@RibbonClient(name = "say-hello", configuration = SayHelloConfiguration.class)的形式
+    ServiceInstance serviceInstance=this.loadBalancerClient.choose("user-ribbon");
+
+    MovieController.log.info("{}:{}:{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance.getPort());
+
+  }
+
+}
